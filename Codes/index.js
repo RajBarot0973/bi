cc2d13d08fd1e733dcad35d2a30c4b330e9ceeaa
RajBarot0973 <138517955+RajBarot0973@users.noreply.github.com>
2024-09-04 22:23:50 +0530

    Add files via upload

---
+++ Codes/index.js
@@ -0,0 +1,4 @@
+document.getElementById('revealBtn').addEventListener('click', function() {
+    document.getElementById('surpriseMessage').classList.remove('hidden');
+    this.style.display = 'none';
+});
